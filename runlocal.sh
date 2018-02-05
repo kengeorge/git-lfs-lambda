34b8920bffeccb7da75ef63cd0b551e8dc5c2f57
Ken George <ken@whatever.email>
2018-02-05 11:40:14 -0800

    Updated runlocal to compose a proper sam environemnt for the whole api.

--- runlocal.sh
+++ runlocal.sh
@@ -1,8 +1,36 @@
 #!/bin/bash
 
 template=template.yaml
-repoName=localRepo
-outTemplate=./tmp/converted.yaml
+repoName=localtest
+outDir="./tmp"
+outTemplate=$outDir/converted.yaml
+funcDir="./src/functions"
+commonDir="./src/common"
 
-sed s/\$\{repoName\}/$repoName/g $template > $outTemplate
-sam local start-api -t $outTemplate
+samDir=$outDir/sam
+if [ -d $samDir ]; then
+    echo "Removing old $samDir..."
+    rm -rf $samDir
+fi
+mkdir -p $samDir
+echo "Copying SAM template..."
+sed s/\$\{repoName\}/$repoName/g $template > $samDir/$template
+
+commonOutDir=$samDir/common
+if [ -d $commonOutDir ]; then
+    echo "Removing old $commonOutDir..."
+    rm -rf $commonOutDir
+fi
+echo "Copying $commonDir to $commonOutDir..."
+mkdir -p  $commonOutDir
+cp -R $commonDir/ $commonOutDir/
+
+names=`ls $funcDir`
+for name in $names; do
+    funcSrcDir=$funcDir/$name
+    echo "Copying $funcSrcDir to $samDir..."
+    cp -R $funcSrcDir/ $samDir/
+done
+
+sam local start-api -t $samDir/template.yaml
+
